81fb49742da2ce767437096c09e7b67b43b90341
James Deery <james.deery@jadex.org>
2013-05-11 18:04:06 +0100

    Update data format and improve interface

--- include/albase/data.h
+++ include/albase/data.h
@@ -15,40 +15,58 @@
 #include "geometry.h"
 #include "vars.h"
 
+#define AL_DATA_TAG(a, b, c, d) ((((d) << 24) | ((c) << 16) | ((b) << 8) | (a)))
+
+typedef int32_t AlDataTag;
+
+static const AlDataTag AL_NO_TAG = 0;
+static const AlDataTag AL_ANY_TAG = 0;
+
 typedef enum {
 	AL_TOKEN_START = 0xFE,
 	AL_TOKEN_END = 0xEF,
-	AL_TOKEN_EOF
 } AlToken;
 
 typedef struct {
 	uint8_t type;
+	bool array;
 	union {
 		bool boolVal;
-		int intVal;
+		int32_t intVal;
 		double doubleVal;
 		Vec2 vec2;
 		Vec3 vec3;
 		Vec4 vec4;
 		Box box;
 		struct {
-			int length;
-			const char *chars;
+			uint32_t length;
+			char *chars;
 		} string;
+		struct {
+			uint32_t length;
+			void *items;
+		} array;
 	} value;
 } AlDataItem;
 
-void al_data_item_init(AlDataItem *item);
-void al_data_item_free(AlDataItem *item);
+typedef struct AlData AlData;
 
-AlError al_data_read(AlStream *stream, AlDataItem *item);
-AlError al_data_read_rest(AlStream *stream, AlVarType type, void *array, int *count);
-AlError al_data_skip_rest(AlStream *stream);
+AlError al_data_init(AlData **data, AlStream *stream);
+void al_data_free(AlData *data);
 
-AlError al_data_write_start(AlStream *stream);
-AlError al_data_write_end(AlStream *stream);
-AlError al_data_write_value(AlStream *stream, AlVarType type, void *value);
-AlError al_data_write_values(AlStream *stream, AlVarType type, void *values, int count);
-AlError al_data_write_string(AlStream *stream, const char *value, int length);
+AlError al_data_read(AlData *data, AlDataItem *item);
+AlError al_data_read_start(AlData *data);
+AlError al_data_read_start_tag(AlData *data, AlDataTag expected, AlDataTag *actual);
+AlError al_data_read_value(AlData *data, AlVarType type, void *value);
+AlError al_data_read_array(AlData *data, AlVarType type, void *values, uint32_t *count);
+AlError al_data_skip_rest(AlData *data);
+
+AlError al_data_write_start(AlData *data);
+AlError al_data_write_end(AlData *data);
+AlError al_data_write_start_tag(AlData *data, AlDataTag tag);
+AlError al_data_write_simple_tag(AlData *data, AlDataTag tag, AlVarType type, void *value);
+AlError al_data_write_value(AlData *data, AlVarType type, void *value);
+AlError al_data_write_string(AlData *data, const char *value, uint32_t length);
+AlError al_data_write_array(AlData *data, AlVarType type, void *values, uint32_t count);
 
 #endif
--- src/albase/data.c
+++ src/albase/data.c
@@ -9,104 +9,140 @@
 
 #include "albase/data.h"
 
-static AlError read_bool(AlStream *stream, bool *result)
+const uint32_t NO_LENGTH = UINT32_MAX;
+
+#define data_read(data, ptr, size) (data)->stream->read((data)->stream, (ptr), (size))
+#define data_write(data, ptr, size) (data)->stream->write((data)->stream, (ptr), (size))
+#define data_seek(data, offset, whence) (data)->stream->seek((data)->stream, (offset), (whence))
+
+struct AlData {
+	AlStream *stream;
+	void *temp;
+};
+
+AlError al_data_init(AlData **result, AlStream *stream)
+{
+	BEGIN()
+
+	AlData *data = NULL;
+	TRY(al_malloc(&data, sizeof(AlData), 1));
+
+	data->stream = stream;
+	data->temp = NULL;
+
+	*result = data;
+
+	PASS()
+}
+
+void al_data_free(AlData *data)
+{
+	if (data) {
+		free(data->temp);
+		free(data);
+	}
+}
+
+static AlError write_type(AlData *data, AlVarType type)
+{
+	uint8_t t = type;
+	return data_write(data, &t, 1);
+}
+
+static AlError read_bool(AlData *data, bool *result)
 {
 	BEGIN()
 
 	uint8_t value;
-	TRY(stream->read(stream, &value, 1));
+	TRY(data_read(data, &value, 1));
 
 	*result = value;
 
 	PASS()
 }
 
-static AlError write_bool(AlStream *stream, bool *value)
+static AlError write_bool(AlData *data, bool *value)
 {
 	uint8_t val = *value;
-	return stream->write(stream, &val, 1);
+	return data_write(data, &val, 1);
 }
 
-static AlError read_int(AlStream *stream, int *result)
+static AlError read_int(AlData *data, int32_t *result)
 {
-	BEGIN()
-
-	int32_t value;
-	TRY(stream->read(stream, &value, 4));
-
-	*result = value;
-
-	PASS()
+	return data_read(data, result, 4);
 }
 
-static AlError write_int(AlStream *stream, int *value)
+static AlError write_int(AlData *data, int32_t *value)
 {
-	int32_t	val = *value;
-	return stream->write(stream, &val, 4);
+	return data_write(data, value, 4);
 }
 
-static AlError read_double(AlStream *stream, double *result)
+static AlError read_double(AlData *data, double *result)
 {
-	return stream->read(stream, result, 8);
+	return data_read(data, result, 8);
 }
 
-static AlError write_double(AlStream *stream, double *value)
+static AlError write_double(AlData *data, double *value)
 {
-	return stream->write(stream, &value, 8);
+	return data_write(data, value, 8);
 }
 
-static AlError read_vec2(AlStream *stream, Vec2 *result)
+static AlError read_vec2(AlData *data, Vec2 *result)
 {
-	return stream->read(stream, result, 16);
+	return data_read(data, result, 16);
 }
 
-static AlError write_vec2(AlStream *stream, Vec2 *value)
+static AlError write_vec2(AlData *data, Vec2 *value)
 {
-	return stream->write(stream, value, 16);
+	return data_write(data, value, 16);
 }
 
-static AlError read_vec3(AlStream *stream, Vec3 *result)
+static AlError read_vec3(AlData *data, Vec3 *result)
 {
-	return stream->read(stream, result, 24);
+	return data_read(data, result, 24);
 }
 
-static AlError write_vec3(AlStream *stream, Vec3 *value)
+static AlError write_vec3(AlData *data, Vec3 *value)
 {
-	return stream->write(stream, value, 24);
+	return data_write(data, value, 24);
 }
 
-static AlError read_vec4(AlStream *stream, Vec4 *result)
+static AlError read_vec4(AlData *data, Vec4 *result)
 {
-	return stream->read(stream, result, 32);
+	return data_read(data, result, 32);
 }
 
-static AlError write_vec4(AlStream *stream, Vec4 *value)
+static AlError write_vec4(AlData *data, Vec4 *value)
 {
-	return stream->write(stream, value, 32);
+	return data_write(data, value, 32);
 }
 
-static AlError read_box(AlStream *stream, Box *result)
+static AlError read_box(AlData *data, Box *result)
 {
-	return stream->read(stream, result, 32);
+	return data_read(data, result, 32);
 }
 
-static AlError write_box(AlStream *stream, Box *value)
+static AlError write_box(AlData *data, Box *value)
 {
-	return stream->write(stream, value, 32);
+	return data_write(data, value, 32);
 }
 
-static AlError read_string(AlStream *stream, char **result, int *resultLength)
+static AlError read_string(AlData *data, char **result, uint32_t *resultLength)
 {
 	BEGIN()
 
 	uint32_t length;
 	char *chars;
-	TRY(stream->read(stream, &length, 4));
+	TRY(data_read(data, &length, 4));
 	TRY(al_malloc(&chars, sizeof(char), length + 1));
-	TRY(stream->read(stream, chars, length));
+	TRY(data_read(data, chars, length));
 	chars[length] = '\0';
 
-	*result = chars;
+	if (data->temp) {
+		free(data->temp);
+	}
+
+	*result = data->temp = chars;
 	if (resultLength) {
 		*resultLength = length;
 	}
@@ -117,59 +153,139 @@ static AlError read_string(AlStream *stream, char **result, int *resultLength)
 	FINALLY()
 }
 
-static AlError write_string(AlStream *stream, const char *value, int length)
+static AlError write_string(AlData *data, const char *value, uint32_t length)
 {
 	BEGIN()
 
 	uint32_t len;
-	if (length > 0) {
+	if (length != NO_LENGTH) {
 		len = length;
 	} else {
 		len = (uint32_t)strlen(value);
 	}
 
-	TRY(stream->write(stream, &len, 4));
-	TRY(stream->write(stream, value, len));
+	TRY(data_write(data, &len, 4));
+	TRY(data_write(data, value, len));
 
 	PASS()
 }
 
-void al_data_item_init(AlDataItem *item)
+static size_t get_var_size(AlVarType type)
 {
-	item->value.string.chars = NULL;
+	switch (type) {
+		case VAR_BOOL: return sizeof(bool);
+		case VAR_INT: return sizeof(int32_t);
+		case VAR_DOUBLE: return sizeof(double);
+		case VAR_VEC2: return sizeof(Vec2);
+		case VAR_VEC3: return sizeof(Vec3);
+		case VAR_VEC4: return sizeof(Vec4);
+		case VAR_BOX: return sizeof(Box);
+		case VAR_STRING: return sizeof(char *);
+		default: return 0;
+	}
 }
 
-void al_data_item_free(AlDataItem *item)
+static AlError read_array(AlData *data, AlVarType type, void *result, uint32_t *resultCount)
 {
-	free((char *)item->value.string.chars);
+	BEGIN()
+
+	size_t itemSize = get_var_size(type);
+	uint32_t length;
+	void *array = NULL;
+	TRY(data_read(data, &length, 4));
+	TRY(al_malloc(&array, itemSize, length));
+
+	for (int i = 0; i < length; i++) {
+		void *item = array + (i * itemSize);
+
+		switch (type) {
+			case VAR_BOOL: TRY(read_bool(data, item)); break;
+			case VAR_INT: TRY(read_int(data, item)); break;
+			case VAR_DOUBLE: TRY(read_double(data, item)); break;
+			case VAR_VEC2: TRY(read_vec2(data, item)); break;
+			case VAR_VEC3: TRY(read_vec3(data, item)); break;
+			case VAR_VEC4: TRY(read_vec4(data, item)); break;
+			case VAR_BOX: TRY(read_box(data, item)); break;
+			default:
+				THROW(AL_ERROR_INVALID_DATA);
+		}
+	}
+
+	if (data->temp) {
+		free(data->temp);
+	}
+
+	*(void **)result = data->temp = array;
+	*resultCount = length;
+
+	CATCH(
+		free(array);
+	)
+	FINALLY()
 }
 
-AlError al_data_read(AlStream *stream, AlDataItem *item)
+static AlError write_array(AlData *data, AlVarType type, void *values, uint32_t count)
+{
+	BEGIN()
+
+	size_t itemSize = get_var_size(type);
+
+	TRY(data_write(data, &count, 4));
+
+	for (int i = 0; i < count; i++) {
+		void *value = values + (i * itemSize);
+
+		switch (type) {
+			case VAR_BOOL: TRY(write_bool(data, value)); break;
+			case VAR_INT: TRY(write_int(data, value)); break;
+			case VAR_DOUBLE: TRY(write_double(data, value)); break;
+			case VAR_VEC2: TRY(write_vec2(data, value)); break;
+			case VAR_VEC3: TRY(write_vec3(data, value)); break;
+			case VAR_VEC4: TRY(write_vec4(data, value)); break;
+			case VAR_BOX: TRY(write_box(data, value)); break;
+			default:
+				THROW(AL_ERROR_INVALID_DATA);
+		}
+	}
+
+	PASS()
+}
+
+AlError al_data_read(AlData *data, AlDataItem *item)
 {
 	BEGIN()
 
 	uint8_t type;
-	TRY(stream->read(stream, &type, 1));
+	TRY(data_read(data, &type, 1));
+
+	item->array = false;
 
 	switch (type) {
 		case AL_TOKEN_START:
 		case AL_TOKEN_END:
 			break;
 
-		case VAR_BOOL: TRY(read_bool(stream, &item->value.boolVal)); break;
-		case VAR_INT: TRY(read_int(stream, &item->value.intVal)); break;
-		case VAR_DOUBLE: TRY(read_double(stream, &item->value.doubleVal)); break;
-		case VAR_VEC2: TRY(read_vec2(stream, &item->value.vec2)); break;
-		case VAR_VEC3: TRY(read_vec3(stream, &item->value.vec3)); break;
-		case VAR_VEC4: TRY(read_vec4(stream, &item->value.vec4)); break;
-		case VAR_BOX: TRY(read_box(stream, &item->value.box)); break;
-
+		case VAR_BOOL: TRY(read_bool(data, &item->value.boolVal)); break;
+		case VAR_INT: TRY(read_int(data, &item->value.intVal)); break;
+		case VAR_DOUBLE: TRY(read_double(data, &item->value.doubleVal)); break;
+		case VAR_VEC2: TRY(read_vec2(data, &item->value.vec2)); break;
+		case VAR_VEC3: TRY(read_vec3(data, &item->value.vec3)); break;
+		case VAR_VEC4: TRY(read_vec4(data, &item->value.vec4)); break;
+		case VAR_BOX: TRY(read_box(data, &item->value.box)); break;
 		case VAR_STRING:
-		{
-			char **chars = (char **)&item->value.string.chars;
-			free(*chars);
-			TRY(read_string(stream, chars, &item->value.string.length));
-		}
+			TRY(read_string(data, &item->value.string.chars, &item->value.string.length));
+			break;
+
+		case VAR_BOOL | 0x80:
+		case VAR_INT | 0x80:
+		case VAR_DOUBLE | 0x80:
+		case VAR_VEC2 | 0x80:
+		case VAR_VEC3 | 0x80:
+		case VAR_VEC4 | 0x80:
+		case VAR_BOX | 0x80:
+			type &= 0x7F;
+			item->array = true;
+			TRY(read_array(data, type, &item->value.array.items, &item->value.array.length));
 			break;
 
 		default:
@@ -181,170 +297,212 @@ AlError al_data_read(AlStream *stream, AlDataItem *item)
 	PASS()
 }
 
-static size_t get_var_size(AlVarType type)
-{
-	switch (type) {
-		case VAR_BOOL: return sizeof(bool);
-		case VAR_INT: return sizeof(int);
-		case VAR_DOUBLE: return sizeof(double);
-		case VAR_VEC2: return sizeof(Vec2);
-		case VAR_VEC3: return sizeof(Vec3);
-		case VAR_VEC4: return sizeof(Vec4);
-		case VAR_BOX: return sizeof(Box);
-		case VAR_STRING: return sizeof(char *);
-		default: return 0;
-	}
-}
-
-AlError al_data_read_rest(AlStream *stream, AlVarType arrayType, void *result, int *resultCount)
+AlError al_data_read_start(AlData *data)
 {
 	BEGIN()
 
-	size_t itemSize = get_var_size(arrayType);
-	int size = (*resultCount) ? *resultCount : 4;
-	void *array = NULL;
-	int count = 0;
+	AlDataItem item;
+	TRY(al_data_read(data, &item));
 
-	TRY(al_malloc(&array, itemSize, size));
+	if (item.type != AL_TOKEN_START)
+		THROW(AL_ERROR_INVALID_DATA);
 
-	while (true) {
-		uint8_t type;
-		TRY(stream->read(stream, &type, 1));
-
-		if (type == AL_TOKEN_START)
-			THROW(AL_ERROR_INVALID_DATA);
-
-		if (type == AL_TOKEN_END)
-			break;
-
-		if (type != arrayType)
-			THROW(AL_ERROR_INVALID_DATA);
-
-		if (count == size) {
-			TRY(al_realloc(&array, itemSize, size * 2));
-			size *= 2;
-		}
-
-		void *item = array + ((count + 1) * itemSize);
-
-		switch (type) {
-			case VAR_BOOL: TRY(read_bool(stream, item)); break;
-			case VAR_INT: TRY(read_int(stream, item)); break;
-			case VAR_DOUBLE: TRY(read_double(stream, item)); break;
-			case VAR_VEC2: TRY(read_vec2(stream, item)); break;
-			case VAR_VEC3: TRY(read_vec3(stream, item)); break;
-			case VAR_VEC4: TRY(read_vec4(stream, item)); break;
-			case VAR_BOX: TRY(read_box(stream, item)); break;
-			case VAR_STRING: TRY(read_string(stream, item, NULL)); break;
-		}
-
-		count++;
-	}
-
-	*(void **)result = array;
-	*resultCount = count;
-
-	CATCH(
-		if (array) {
-		  if (arrayType == VAR_STRING) {
-			  for (int i = 0; i < count; i++) {
-				  free(((char *)array)[i]);
-			  }
-		  }
-
-		  free(array);
-		}
-	)
-	FINALLY()
+	PASS()
 }
 
-AlError al_data_skip_rest(AlStream *stream)
+AlError al_data_read_start_tag(AlData *data, AlDataTag expected, AlDataTag *actual)
+{
+	BEGIN()
+
+	AlDataItem item;
+	int tag;
+
+	TRY(al_data_read(data, &item));
+
+	switch (item.type) {
+		case AL_TOKEN_START:
+			TRY(al_data_read_value(data, VAR_INT, &tag))
+
+			if (expected != AL_ANY_TAG && tag != expected)
+				THROW(AL_ERROR_INVALID_DATA);
+
+			if (actual) {
+				*actual = tag;
+			}
+			break;
+
+		case AL_TOKEN_END:
+			if (expected)
+				THROW(AL_ERROR_INVALID_DATA);
+
+			*actual = AL_NO_TAG;
+			break;
+
+		default:
+			THROW(AL_ERROR_INVALID_DATA);
+	}
+	
+	PASS()
+}
+
+AlError al_data_read_value(AlData *data, AlVarType type, void *value)
+{
+	BEGIN()
+
+	AlDataItem item;
+	TRY(al_data_read(data, &item));
+	if (item.type != type || item.array)
+		THROW(AL_ERROR_INVALID_DATA);
+
+	switch (type) {
+		case VAR_BOOL: *(bool *)value = item.value.boolVal; break;
+		case VAR_INT: *(int *)value = item.value.intVal; break;
+		case VAR_DOUBLE: *(double *)value = item.value.doubleVal; break;
+		case VAR_VEC2: *(Vec2 *)value = item.value.vec2; break;
+		case VAR_VEC3: *(Vec3 *)value = item.value.vec3; break;
+		case VAR_VEC4: *(Vec4 *)value = item.value.vec4; break;
+		case VAR_BOX: *(Box *)value = item.value.box; break;
+		case VAR_STRING: *(const char **)value = item.value.string.chars; break;
+	}
+
+	PASS()
+}
+
+AlError al_data_read_array(AlData *data, AlVarType type, void *values, uint32_t *count)
+{
+	BEGIN()
+
+	AlDataItem item;
+	TRY(al_data_read(data, &item));
+	if (item.type != type || !item.array)
+		THROW(AL_ERROR_INVALID_DATA);
+
+	*(void **)values = item.value.array.items;
+	*count = item.value.array.length;
+	data->temp = NULL;
+
+	PASS()
+}
+
+AlError al_data_skip_rest(AlData *data)
 {
 	BEGIN()
 
 	bool atEnd = false;
-
-	while (!atEnd) {
+	do {
 		uint8_t type;
-		TRY(stream->read(stream, &type, 1));
+		TRY(data_read(data, &type, 1));
 
 		switch (type) {
 			case AL_TOKEN_START:
-				al_data_skip_rest(stream);
+				al_data_skip_rest(data);
 				break;
 
 			case AL_TOKEN_END:
 				atEnd = true;
 				break;
 
-			case VAR_BOOL: TRY(stream->seek(stream, 1, AL_SEEK_CUR)); break;
-			case VAR_INT: TRY(stream->seek(stream, 4, AL_SEEK_CUR)); break;
-			case VAR_DOUBLE: TRY(stream->seek(stream, 8, AL_SEEK_CUR)); break;
-			case VAR_VEC2: TRY(stream->seek(stream, 16, AL_SEEK_CUR)); break;
-			case VAR_VEC3: TRY(stream->seek(stream, 24, AL_SEEK_CUR)); break;
-			case VAR_VEC4: TRY(stream->seek(stream, 32, AL_SEEK_CUR)); break;
-			case VAR_BOX: TRY(stream->seek(stream, 32, AL_SEEK_CUR)); break;
+			case VAR_BOOL: TRY(data_seek(data, 1, AL_SEEK_CUR)); break;
+			case VAR_INT: TRY(data_seek(data, 4, AL_SEEK_CUR)); break;
+			case VAR_DOUBLE: TRY(data_seek(data, 8, AL_SEEK_CUR)); break;
+			case VAR_VEC2: TRY(data_seek(data, 16, AL_SEEK_CUR)); break;
+			case VAR_VEC3: TRY(data_seek(data, 24, AL_SEEK_CUR)); break;
+			case VAR_VEC4: TRY(data_seek(data, 32, AL_SEEK_CUR)); break;
+			case VAR_BOX: TRY(data_seek(data, 32, AL_SEEK_CUR)); break;
 
 			case VAR_STRING:
+			case VAR_BOOL | 0x80:
+			case VAR_INT | 0x80:
+			case VAR_DOUBLE | 0x80:
+			case VAR_VEC2 | 0x80:
+			case VAR_VEC3 | 0x80:
+			case VAR_VEC4 | 0x80:
+			case VAR_BOX | 0x80:
 			{
 				uint32_t length;
-				TRY(stream->read(stream, &length, 4));
-				TRY(stream->seek(stream, length, AL_SEEK_CUR));
+				TRY(data_read(data, &length, 4));
+				TRY(data_seek(data, length, AL_SEEK_CUR));
 			}
 				break;
 
 			default:
 				THROW(AL_ERROR_INVALID_DATA);
 		}
-	}
+	} while (!atEnd);
 
 	PASS()
 }
 
-AlError al_data_write_start(AlStream *stream)
+AlError al_data_write_start(AlData *data)
 {
 	uint8_t start = AL_TOKEN_START;
-	return stream->write(stream, &start, 1);
+	return data_write(data, &start, 1);
 }
 
-AlError al_data_write_end(AlStream *stream)
+AlError al_data_write_end(AlData *data)
 {
 	uint8_t end = AL_TOKEN_END;
-	return stream->write(stream, &end, 1);
+	return data_write(data, &end, 1);
 }
 
-AlError al_data_write_value(AlStream *stream, AlVarType type, void *value)
+AlError al_data_write_start_tag(AlData *data, AlDataTag tag)
 {
 	BEGIN()
+
+	TRY(al_data_write_start(data));
+	TRY(write_type(data, VAR_INT));
+	TRY(write_int(data, &tag));
+
+	PASS()
+}
+
+AlError al_data_write_simple_tag(AlData *data, AlDataTag tag, AlVarType type, void *value)
+{
+	BEGIN()
+
+	TRY(al_data_write_start_tag(data, tag));
+	TRY(al_data_write_value(data, type, value));
+	TRY(al_data_write_end(data));
+
+	PASS()
+}
+
+AlError al_data_write_value(AlData *data, AlVarType type, void *value)
+{
+	BEGIN()
+
+	TRY(write_type(data, type));
 
 	switch (type) {
-		case VAR_BOOL: TRY(write_bool(stream, value)); break;
-		case VAR_INT: TRY(write_int(stream, value)); break;
-		case VAR_DOUBLE: TRY(write_double(stream, value)); break;
-		case VAR_VEC2: TRY(write_vec2(stream, value)); break;
-		case VAR_VEC3: TRY(write_vec3(stream, value)); break;
-		case VAR_VEC4: TRY(write_vec4(stream, value)); break;
-		case VAR_BOX: TRY(write_box(stream, value)); break;
-		case VAR_STRING: TRY(write_string(stream, value, -1)); break;
+		case VAR_BOOL: TRY(write_bool(data, value)); break;
+		case VAR_INT: TRY(write_int(data, value)); break;
+		case VAR_DOUBLE: TRY(write_double(data, value)); break;
+		case VAR_VEC2: TRY(write_vec2(data, value)); break;
+		case VAR_VEC3: TRY(write_vec3(data, value)); break;
+		case VAR_VEC4: TRY(write_vec4(data, value)); break;
+		case VAR_BOX: TRY(write_box(data, value)); break;
+		case VAR_STRING: TRY(write_string(data, value, NO_LENGTH)); break;
 	}
 
 	PASS()
 }
 
-AlError al_data_write_values(AlStream *stream, AlVarType type, void *values, int count)
+AlError al_data_write_string(AlData *data, const char *value, uint32_t length)
 {
 	BEGIN()
 
-	size_t itemSize = get_var_size(type);
-
-	for (int i = 0; i < count; i++) {
-		TRY(al_data_write_value(stream, type, values + (i * itemSize)));
-	}
+	TRY(write_type(data, VAR_STRING));
+	TRY(write_string(data, value, length));
 
 	PASS()
 }
 
-AlError al_data_write_string(AlStream *stream, const char *value, int length)
+AlError al_data_write_array(AlData *data, AlVarType type, void *values, uint32_t count)
 {
-	return write_string(stream, value, length);
+	BEGIN()
+
+	TRY(write_type(data, type | 0x80));
+	TRY(write_array(data, type, values, count));
+
+	PASS()
 }
