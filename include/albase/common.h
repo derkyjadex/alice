0c7752854f072a404b32db2ad941bf89bf0d84bc
James Deery <james.deery@jadex.org>
2013-10-06 23:21:32 +0100

    Delete unused FOR_EACH macro

--- include/albase/common.h
+++ include/albase/common.h
@@ -11,9 +11,6 @@
 
 #include "albase/error.h"
 
-#define FOR_EACH(type, item, initial) \
-	for (type **(item ## Iter) = (initial), *item = *(item ## Iter); item; item = *(++(item ## Iter)))
-
 AlError al_malloc(void *ptr, size_t size);
 AlError al_realloc(void *ptr, size_t size);
 
