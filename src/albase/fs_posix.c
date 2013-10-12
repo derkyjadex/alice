d11ab26f1b06d7702c9c142b7da418e83034bde5
James Deery <james.deery@jadex.org>
2013-10-12 15:18:06 +0100

    Add dummy fs implementation for standard POSIX systems

---
+++ src/albase/fs_posix.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2013 James Deery
+ * Released under the MIT license <http://opensource.org/licenses/MIT>.
+ * See COPYING for details.
+ */
+
+#include "albase/fs.h"
+
+AlError al_fs_chdir_app_data()
+{
+	BEGIN()
+	PASS()
+}
